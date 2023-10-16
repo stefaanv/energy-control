5a42589015d97ea430923c50112ad2fd149cc811
Stefaan Vandevelde <stefaan.vandevelde.kortrijk@gmail.com>
2023-10-16 08:15:00 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  Hallo {{ teller }}
+  <v-btn @click="clicked">tekst</v-btn>
+  <v-date-picker v-show="true"></v-date-picker>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+const teller = ref(0)
+function clicked() {
+  teller.value++
+  console.log(teller.value)
+}
+</script>
