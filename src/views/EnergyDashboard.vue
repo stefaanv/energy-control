d024d3e503785681c58c4e47dc52439c140c4636
Stefaan Vandevelde <stefaan.vandevelde.kortrijk@gmail.com>
2023-10-22 17:36:25 +0200

    Cleanup

--- src/views/EnergyDashboard.vue
+++ src/views/EnergyDashboard.vue
@@ -1,31 +1,29 @@
 <template>
   <div class="table-container">
     <v-table density="compact">
-      <Suspense>
-        <thead>
-          <tr>
-            <th class="bold text-center" colspan="5" style="font-weight: 800; font-size: larger">
-              Charge/discharge tasks
-            </th>
-          </tr>
-          <tr>
-            <th class="text-center" style="font-weight: 800">Dag</th>
-            <th class="text-center" style="font-weight: 800">Soort</th>
-            <th class="text-center" style="font-weight: 800">Van</th>
-            <th class="text-center" style="font-weight: 800">Tot</th>
-            <th class="text-center" style="font-weight: 800">Vermogen [W]</th>
-          </tr>
-        </thead>
-        <tbody>
-          <charge-task v-for="(task, index) of taskList" v-bind:key="index" v-model:task="taskList[index]"
-            @update:task="console.log(toRaw(taskList[index]))"></charge-task>
-        </tbody>
-      </Suspense>
+      <thead>
+        <tr>
+          <th class="bold text-center" colspan="5" style="font-weight: 800; font-size: larger">
+            Charge/discharge tasks
+          </th>
+        </tr>
+        <tr>
+          <th class="text-center" style="font-weight: 800">Dag</th>
+          <th class="text-center" style="font-weight: 800">Soort</th>
+          <th class="text-center" style="font-weight: 800">Van</th>
+          <th class="text-center" style="font-weight: 800">Tot</th>
+          <th class="text-center" style="font-weight: 800">Vermogen [W]</th>
+        </tr>
+      </thead>
+      <tbody>
+        <charge-task v-for="(task, index) of taskList" v-bind:key="index" v-model:task="taskList[index]"
+          @update:task="console.log(toRaw(taskList[index]))"></charge-task>
+      </tbody>
     </v-table>
   </div>
 </template>
 <script lang="ts" setup>
-import { Ref, ref, toRaw, Suspense } from 'vue'
+import { Ref, ref, toRaw } from 'vue'
 import { IChargeTask } from '@/shared-models/charge-task.interface'
 import ChargeTask from '@/components/ChargeTask.vue'
 import Axios from 'axios';
